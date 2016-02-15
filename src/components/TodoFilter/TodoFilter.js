d0dfdcddffeedf117907a989ddf4391e9443e14a
Stefan Wille <github@stefanwille.com>
2016-02-15 10:20:31 +0100

    Refactor TodoFilter into a function

--- src/components/TodoFilter/TodoFilter.js
+++ src/components/TodoFilter/TodoFilter.js
@@ -1,42 +1,30 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
 
-class TodoFilter extends React.Component {
-  static propTypes = {
-    todos: PropTypes.array.isRequired,
-    todoFilter: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired
-  };
+const labels = {ALL: 'All', ACTIVE: 'Active', COMPLETED: 'Completed'}
 
-  render () {
-    const labels = {ALL: 'All', ACTIVE: 'Active', COMPLETED: 'Completed'}
-
-    return (
-      <div className='todo-filter'>
-
-        <label className='control-label input-group'>Filter</label>
-        <div className='btn-group' data-toggle='buttons'>
-          {
-            ['ALL', 'ACTIVE', 'COMPLETED'].map((todoFilter) => {
-              let className = 'btn btn-default'
-              let label = labels[todoFilter]
-              if (todoFilter === this.props.todoFilter) {
-                className += ' active'
-              }
-              return (
-                <label key={todoFilter} className={className}>
-                  <input name='year'
-                          value={todoFilter}
-                          type='radio'
-                          onClick={this.props.onChange.bind(this, todoFilter)}/>{label}
-                </label>
-              )
-            })
+const TodoFilter = ({todos, todoFilter, onChange}) => (
+  <div className='todo-filter'>
+    <label className='control-label input-group'>Filter</label>
+    <div className='btn-group' data-toggle='buttons'>
+      {
+        ['ALL', 'ACTIVE', 'COMPLETED'].map((todoFilter) => {
+          let className = 'btn btn-default'
+          if (todoFilter === this.props.todoFilter) {
+            className += ' active'
           }
-        </div>
-      </div>
-    )
-  }
-}
+          const label = labels[todoFilter]
+          return (
+            <label key={todoFilter} className={className}>
+              <input name='year'
+                      value={todoFilter}
+                      type='radio'
+                      onClick={this.props.onChange.bind(this, todoFilter)}/>{label}
+            </label>
+          )
+        })
+      }
+    </div>
+  </div>
+)
 
 export default TodoFilter
-
