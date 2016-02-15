d94372c44c92534089bc7b6b1b657f1f60dc28af
Stefan Wille <github@stefanwille.com>
2016-02-15 10:22:40 +0100

    Refactor TodoList into a function

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,35 +1,25 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
 
 import Todo from 'components/Todo/Todo'
 
-class TodoList extends React.Component {
-  static propTypes = {
-    todos: PropTypes.array.isRequired,
-    onCompleted: PropTypes.func.isRequired,
-    onDelete: PropTypes.func.isRequired,
-    onDeleteButtonVisibilityChanged: PropTypes.func.isRequired,
-    deleteButtonOnTodo: PropTypes.number.isRequired
-  };
-
-  render () {
-    const todosComponents = this.props.todos.map(todo => (
-                                <Todo todo={todo}
-                                      onCompleted={this.props.onCompleted}
-                                      onDelete={this.props.onDelete}
-                                      key={todo.id}
-                                      onDeleteButtonVisibilityChanged={this.props.onDeleteButtonVisibilityChanged}
-                                      deleteButtonVisible={todo.id === this.props.deleteButtonOnTodo}
-                                />
-                            )
-    )
-    return (
-      <table className='todo-list todo-table'>
-        <tbody>
-          { todosComponents }
-        </tbody>
-      </table>
-    )
-  }
+const TodoList = ({todos, onCompleted, onDelete, onDeleteButtonVisibilityChanged, deleteButtonOnTodo}) => {
+  const todosComponents = this.props.todos.map(todo => (
+                              <Todo todo={todo}
+                                    onCompleted={this.props.onCompleted}
+                                    onDelete={this.props.onDelete}
+                                    key={todo.id}
+                                    onDeleteButtonVisibilityChanged={this.props.onDeleteButtonVisibilityChanged}
+                                    deleteButtonVisible={todo.id === this.props.deleteButtonOnTodo}
+                              />
+                          )
+  )
+  return (
+    <table className='todo-list todo-table'>
+      <tbody>
+        { todosComponents }
+      </tbody>
+    </table>
+  )
 }
 
 export default TodoList
