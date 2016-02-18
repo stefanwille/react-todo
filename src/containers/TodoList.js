630d600e3bd0804be52b2d3c72771ac29a7c7a9c
Stefan Wille <github@stefanwille.com>
2016-02-18 17:08:31 +0100

    Improve filter predicate

--- src/containers/TodoList.js
+++ src/containers/TodoList.js
@@ -2,27 +2,22 @@ import {connect} from 'react-redux'
 
 import TodoListPresentation from 'components/TodoListPresentation/TodoListPresentation'
 
-const getVisibleTodos = (todos, todoFilter) => {
-  const predicate = getTodoFilterPredicate(todoFilter)
-  return todos.filter(predicate)
-}
-
-const getTodoFilterPredicate = (todoFilter) => {
-  switch (todoFilter) {
-    case 'ALL':
-      return (v) => v
-    case 'ACTIVE':
-      return (v) => !v.completed
-    case 'COMPLETED':
-      return (v) => v.completed
-    default:
-      throw new Error('Unknown todoFilter' + todoFilter)
-  }
-}
-
 const mapStateToProps = (state) => {
   return {
     todos: getVisibleTodos(state.todos, state.todoFilter)
+  }
+}
+
+const getVisibleTodos = (todos, todoFilter) => {
+  switch (todoFilter) {
+    case 'ALL':
+      return todos
+
+    case 'ACTIVE':
+      return todos.filter(v => !v.completed)
+
+    case 'COMPLETED':
+      return todos.filter(v => v.completed)
   }
 }
 
