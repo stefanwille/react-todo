311a23f8674bb305d8e90cd2339eab8a4b06d98a
Stefan Wille <github@stefanwille.com>
2016-02-28 08:13:54 +0100

    Introduce RadioSelection.Option

--- src/components/RadioSelection/RadioSelection.js
+++ src/components/RadioSelection/RadioSelection.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {PropTypes} from 'react'
 
 const RadioSelection = ({children}) => (
   <div className='btn-group' data-toggle='buttons'>
@@ -7,6 +7,28 @@ const RadioSelection = ({children}) => (
 )
 
 RadioSelection.propTypes = {
+}
+
+RadioSelection.Option = ({option, label, active, onChange}) => {
+  let className = 'btn btn-default'
+  if (active) {
+    className += ' active'
+  }
+  return (
+    <label key={option} className={className}>
+      <input
+              value={option}
+              type='radio'
+              onClick={() => onChange(option)}/>{label}
+    </label>
+  )
+}
+
+RadioSelection.Option.propTypes = {
+  option: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  active: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired
 }
 
 export default RadioSelection
--- src/containers/TodoFilter.js
+++ src/containers/TodoFilter.js
@@ -3,7 +3,6 @@ import {connect} from 'react-redux'
 
 import {selectTodoFilter} from 'actions/actionCreators'
 import RadioSelection from 'components/RadioSelection/RadioSelection'
-import RadioSelectionOption from 'components/RadioSelectionOption/RadioSelectionOption'
 
 const mapStateToProps = (state) => {
   return {
@@ -23,9 +22,9 @@ const TodoFilterPresentation = ({selected, onChange}) => (
   <div className='todo-filter'>
     <label className='control-label input-group'>Filter</label>
     <RadioSelection>
-      <RadioSelectionOption option='ALL' label='All' active={selected === 'ALL'} onChange={onChange} />
-      <RadioSelectionOption option='ACTIVE' label='Active' active={selected === 'ACTIVE'} onChange={onChange} />
-      <RadioSelectionOption option='COMPLETED' label='Completed' active={selected === 'COMPLETED'} onChange={onChange} />
+      <RadioSelection.Option option='ALL' label='All' active={selected === 'ALL'} onChange={onChange} />
+      <RadioSelection.Option option='ACTIVE' label='Active' active={selected === 'ACTIVE'} onChange={onChange} />
+      <RadioSelection.Option option='COMPLETED' label='Completed' active={selected === 'COMPLETED'} onChange={onChange} />
     </RadioSelection>
   </div>
 )
